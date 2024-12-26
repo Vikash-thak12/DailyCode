ce39550dc0527b364c795cf8ff2053f1b723b65d
Vikash Thakur <thakvikash622@gmail.com>
2024-12-26 23:47:11 +0530

    Fixing: NavItems

--- floating_dock/app/page.tsx
+++ floating_dock/app/page.tsx
@@ -1,11 +1,58 @@
 import React from 'react'
+import { IconBrandGithub, IconBrandTwitter, IconHome, IconNewSection, IconTerminal2 } from "@tabler/icons-react"
+
+export const navitems = [
+  {
+    title: "Home", 
+    icon: <IconHome className='w-full h-full text-neutral-500' />,
+    href: "/"
+  },
+  {
+    title: "Products", 
+    icon: <IconTerminal2 className='w-full h-full text-neutral-500' />,
+    href: "/products"
+  },
+  {
+    title: "Components", 
+    icon: <IconNewSection className='w-full h-full text-neutral-500' />,
+    href: "/components"
+  },
+  {
+    title: "Aceternity UI", 
+    icon: <IconHome className='w-full h-full text-neutral-500' />,
+    href: "/aceternity"
+  },
+  {
+    title: "Github", 
+    icon: <IconBrandGithub className='w-full h-full text-neutral-500' />,
+    href: "/github"
+  },
+  {
+    title: "Twitter", 
+    icon: <IconBrandTwitter className='w-full h-full text-neutral-500' />,
+    href: "/twitter"
+  },
+]
 
 const Homepage = () => {
   return (
     <div className='flex items-center justify-center h-screen'>
-      Home
+      <FloatingDock />
     </div>
   )
 }
 
 export default Homepage
+
+
+const FloatingDock = () => (
+  <div className='fixed bottom-20 mx-auto flex items-center gap-5'>
+    {
+      navitems.map((item,idx) => (
+        <div key={idx}>
+          {item.title}
+        </div>
+      ))
+    }
+  </div>
+);
---
+++ floating_dock/types/index.ts
@@ -0,0 +1,9 @@
+import { IconHome} from "@tabler/icons-react"
+
+export const NavItems = [
+    {
+        title: "Home",
+        icon: <IconHome />,
+        href: "/"
+    }
+]
