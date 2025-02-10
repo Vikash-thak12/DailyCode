be3cf35e66dcf3b5baa990d2c0f75b5fc79ac506
Vikash Thakur <thakvikash622@gmail.com>
2025-02-11 00:38:04 +0530

    Implemented: css

--- Framer_Motion/button/components/Button.tsx
+++ Framer_Motion/button/components/Button.tsx
@@ -2,7 +2,13 @@ import React from 'react'
 
 const Button = () => {
     return (
-        <div className='h-screen w-full bg-neutral-950 flex items-center justify-center'>
+        <div className='h-screen w-full bg-neutral-900 flex items-center justify-center'
+        style={{
+            backgroundImage: `radial-gradient(circle, rgba(6, 182, 212, 0.2) 1px, transparent 0)`, 
+            backgroundSize: "8px 8px",
+            backgroundRepeat: "repeat"
+        }}
+        >
             <button className='text-neutral-400 text-3xl px-8 py-4 bg-gray-800 rounded-lg relative group'>
                 <span> Vikash Thakur</span>
                 <span className='absolute inset-x-0 bottom-px bg-gradient-to-r from-transparent via-cyan-500 to-transparent h-[2px] w-3/4 mx-auto'></span>
