755b17084e67af33ea71eeb91c442bbd6b3711d7
Vikash Thakur <thakvikash622@gmail.com>
2025-02-11 00:01:11 +0530

    Minor changes

--- floating_dock/app/page.tsx
+++ floating_dock/app/page.tsx
@@ -1,34 +1,41 @@
-import React from 'react'
+/* eslint-disable @typescript-eslint/no-explicit-any */
+/* eslint-disable @typescript-eslint/no-unused-vars */
+"use client"
+import React, { useRef } from 'react'
 import { IconBrandGithub, IconBrandTwitter, IconHome, IconNewSection, IconTerminal2 } from "@tabler/icons-react"
+import { useMotionValue, useTransform } from 'motion/react'
+import { motion, MotionValue } from 'motion/react'
+// import { MotionValue } from 'motion'
+import Link from 'next/link'
 
 export const navitems = [
   {
-    title: "Home", 
+    title: "Home",
     icon: <IconHome className='w-full h-full text-neutral-500' />,
     href: "/"
   },
   {
-    title: "Products", 
+    title: "Products",
     icon: <IconTerminal2 className='w-full h-full text-neutral-500' />,
     href: "/products"
   },
   {
-    title: "Components", 
+    title: "Components",
     icon: <IconNewSection className='w-full h-full text-neutral-500' />,
     href: "/components"
   },
   {
-    title: "Aceternity UI", 
+    title: "Aceternity UI",
     icon: <IconHome className='w-full h-full text-neutral-500' />,
     href: "/aceternity"
   },
   {
-    title: "Github", 
+    title: "Github",
     icon: <IconBrandGithub className='w-full h-full text-neutral-500' />,
     href: "/github"
   },
   {
-    title: "Twitter", 
+    title: "Twitter",
     icon: <IconBrandTwitter className='w-full h-full text-neutral-500' />,
     href: "/twitter"
   },
@@ -45,27 +52,54 @@ const Homepage = () => {
 export default Homepage
 
 
-const FloatingDock = () => (
-  <div className='fixed bottom-20 mx-auto inset-x-0 flex items-center justify-center gap-5 bg-neutral-100 w-fit px-4 py-2 rounded-lg'>
-    {
-      navitems.map((item,idx) => (
-        <IconContainer key={idx} item={item} />
-      ))
-    }
-  </div>
-);
+const FloatingDock = () => {
+
+  const mouseX = useMotionValue(Infinity)
+  return (
+    <motion.div
+      onMouseEnter={(e) => mouseX.set(e.pageX)}
+      onMouseLeave={(e) => mouseX.set(Infinity)}
+      className='fixed bottom-20 mx-auto inset-x-0 flex items-center justify-center gap-5 bg-neutral-100 w-fit px-4 py-2 rounded-lg h-16'>
+      {
+        navitems.map((item, idx) => (
+          <IconContainer mouseX={mouseX} key={idx} item={item} />
+        ))
+      }
+    </motion.div>
+  )
+}
 
 interface Link {
-  title: string, 
-  icon: React.ReactNode, 
+  title: string,
+  icon: React.ReactNode,
   href: string
 }
 
 
-export const IconContainer = ({ item }: {item: Link}) => {
+export const IconContainer = ({ item, mouseX }: { item: Link, mouseX: MotionValue<number> }) => {
+  const ref = useRef<HTMLDivElement>(null);
+
+  const distance = useTransform(mouseX, (val) => {
+    if (!ref.current) return Infinity; // Guard against null ref
+    const bounds = ref.current.getBoundingClientRect();
+    return val - bounds.x - bounds.width / 2;
+  });
+
+  const widthTransform = useTransform(distance, [-150, 0, 150], [40, 80, 40]);
+  const heightTransform = useTransform(distance, [-150, 0, 150], [40, 80, 40]);
+
   return (
-    <div>
-      {item.icon}
-    </div>
-  )
-}
+    <Link href={item.href}>
+      <motion.div
+        ref={ref}
+        style={{
+          width: widthTransform,
+          height: heightTransform,
+        }}
+        className="bg-neutral-300 rounded-full"
+      >
+        {item.icon}
+      </motion.div>
+    </Link>
+  );
+};
